88ea0d3a74d2bbd1ed7326fe05644a812bfac650
Alejandro Flores <sjafs.83@gmail.com>
2021-09-09 10:02:12 -0300

    deploy 11

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router} from 'react-router-dom';
+//import { HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import axios from "axios";
@@ -14,11 +14,9 @@ axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
 	<Provider store={store}>
-		<Router>
 			<React.StrictMode>
 				<App />
 			</React.StrictMode>
-		</Router>
 	</Provider>,
 	document.getElementById('root'),
 );
